725cb104aeb685ceb491f968692e332169c08a42
Dietmar Duft <dietmar@duft.it>
2024-01-10 17:00:15 +0100

    added frontend env

--- frontend/src/services/configs/axiosConfigs.ts
+++ frontend/src/services/configs/axiosConfigs.ts
@@ -7,8 +7,6 @@ export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 })
 
-console.log(process.env.REACT_APP_API_URL)
-
 api.interceptors.request.use((config) => requestAuthHandler(config))
 api.interceptors.response.use(undefined, (error) => responseErrorHandler(error))
 api.interceptors.response.use((response) => responseDateHandler(response))
